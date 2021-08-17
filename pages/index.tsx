fe06041b03a70d6edc533859fd0848791f1d81fa
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-08-17 15:12:52 -0300

    fix: corrigir indentação

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,17 @@ const Home: React.FC = () => {
 
       <main className={styles.home}>
         <Post titulo="VESTIBULINHO 2022">
-        <h2>Inscrições de 15/09 a 15/10/2021</h2>
-        <h2>através do link da <a href="www.paulinia.sp.gov.br">prefeitura</a></h2>
-        <h2>Veja todas as informações no <a href="http://www.paulinia.sp.gov.br/uploads/semanarios/1671.pdf">edital completo</a></h2>
-        <h2>a partir da página 3</h2>
+          <h2>Inscrições de 15/09 a 15/10/2021</h2>
+          <h2>
+            através do link da <a href="www.paulinia.sp.gov.br">prefeitura</a>
+          </h2>
+          <h2>
+            Veja todas as informações no{' '}
+            <a href="http://www.paulinia.sp.gov.br/uploads/semanarios/1671.pdf">
+              edital completo
+            </a>
+          </h2>
+          <h2>a partir da página 3</h2>
         </Post>
 
         <Post titulo="Informes agosto/2021">
@@ -23,10 +30,13 @@ const Home: React.FC = () => {
               A escola está aberta de segunda a sexta-feira das 8h00 às 17h00
               para atender a comunidade escolar
             </li>
-            <li>Aulas presenciais no período da manhã, conforme cronograma já divulgado, com a presença de 50% dos alunos em cada semana</li>
             <li>
-              A escola segue com aulas online pelo Google Meet, no período da tarde, e atividades
-              remotas através do Google Sala de Aula
+              Aulas presenciais no período da manhã, conforme cronograma já
+              divulgado, com a presença de 50% dos alunos em cada semana
+            </li>
+            <li>
+              A escola segue com aulas online pelo Google Meet, no período da
+              tarde, e atividades remotas através do Google Sala de Aula
             </li>
             <li>Para contato: sec.cemep@gmail.com</li>
           </ul>
