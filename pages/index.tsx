19141c24f9573b20ad2316067c635750ea9d6afb
SCHMIDTWAGNER <69174411+SCHMIDTWAGNER@users.noreply.github.com>
2021-09-15 12:15:57 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
       <main className={styles.home}>
         <Post titulo="VESTIBULINHO 2022">
           <h2>Inscrições de 15/09 a 15/10/2021 no link abaixo</h2>
-          <h2>http://sistemas1.paulinia.sp.gov.br/insccemepetep/</h2>
+          <h4><a href="http://sistemas1.paulinia.sp.gov.br/insccemepetep/">http://sistemas1.paulinia.sp.gov.br/insccemepetep/</a></h4>
           <h2>
             Veja todas as informações no{' '}
             <a href="http://www.paulinia.sp.gov.br/uploads/semanarios/1671.pdf">
