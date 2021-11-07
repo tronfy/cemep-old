b68f8edb42a379ae2b41c5256610f967152509b3
SCHMIDTWAGNER <69174411+SCHMIDTWAGNER@users.noreply.github.com>
2021-11-06 22:10:27 -0300

    Update vestibulinho.tsx

--- pages/vestibulinho.tsx
+++ pages/vestibulinho.tsx
@@ -11,17 +11,17 @@ const Vestibulinho: React.FC = () => {
       <main className={styles.vestibulinho}>
         <h1>Vestibulinho CEMEP 2022</h1>
         <h2>
-          <u>
-            Inscrições de 15/09 a 15/10/2021:{' '}
-            <a href="http://sistemas1.paulinia.sp.gov.br/insccemepetep/">
-              clique aqui
-            </a>
-          </u>
+          Inscrições indeferidas:{' '}
+          <a href="Indeferidas.pdf">
+            veja aqui
+          </a>
+          <p>
+          Inscrições de 15/09 a 15/10/2021 (encerradas)
         </h2>
         <h2>
-          Veja todas as informações no{' '}
-          <a href="http://www.paulinia.sp.gov.br/uploads/semanarios/1671.pdf">
-            edital completo
+          Veja o Edital completo{' '}
+          <a href="Edital Vestibulinho 2022.pdf">
+            aqui
           </a>
         </h2>
         <h4>(a partir da página 3)</h4>
