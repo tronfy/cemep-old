983e58793a6653e15bad414a01a4de0c7d4d1c89
SCHMIDTWAGNER <69174411+SCHMIDTWAGNER@users.noreply.github.com>
2022-02-01 21:43:14 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,35 +9,17 @@ const Home: React.FC = () => {
   return (
     <>
       <Head title="Home" />
+      <h2>
+        Início das aulas: 08/02/2022
+      </h2>
+      <br />
+      <br />
 
       <main className={styles.home}>
         <Vestibulinho />
         <br />
 
-        <Post titulo="Informes agosto/2021">
-          <ul>
-            <li>
-              A escola está aberta de segunda a sexta-feira das 8h00 às 17h00
-              para atender a comunidade escolar
-            </li>
-            <li>
-              Aulas presenciais no período da manhã, conforme cronograma já
-              divulgado, com a presença de 50% dos alunos em cada semana
-            </li>
-            <li>
-              A escola segue com aulas online pelo Google Meet, no período da
-              tarde, e atividades remotas através do Google Sala de Aula
-            </li>
-            <li>Para contato: sec.cemep@gmail.com</li>
-          </ul>
-        </Post>
-
-        <Post titulo="Ciclo de Palestras e I Colóquio">
-          Realizado de 24 a 28/08/2020 em comemoração aos 30 anos do Cemep{' '}
-          <br />
-          Para ver a lista de presença{' '}
-          <a href="Lista_presenca.pdf">clique aqui</a>.
-        </Post>
+        
       </main>
     </>
   )
