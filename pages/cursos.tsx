90a8189d1820d035bb6bcbf64dde4bbfceeeae7c
SCHMIDTWAGNER <69174411+SCHMIDTWAGNER@users.noreply.github.com>
2022-08-26 07:46:54 -0300

    Update cursos.tsx

--- pages/cursos.tsx
+++ pages/cursos.tsx
@@ -9,16 +9,16 @@ const Cursos: React.FC = () => {
       <Head title="Cursos" />
 
       <main className={styles.cursos}>
-        <h1>Curso Oferecido para 2022</h1>
+        <h1>Curso Oferecido para 2023</h1>
         <h2>PERÍODO INTEGRAL (manhã e tarde)</h2>
 
         <h3>Ensino Médio concomitante com Técnico em Informática</h3>
         <p>Duração do Curso: 3 anos</p>
         <p>
-          Curso destinado a alunos concluintes em 2021 no Ensino Fundamental
+          Curso destinado a alunos concluintes em 2022 no Ensino Fundamental
         </p>
         <p>
-          <b>80 vagas</b>
+          <b>80 vagas + 3 p/ educação especial</b>
         </p>
       </main>
     </>
