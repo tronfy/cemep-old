009829dcd7a24e3b9f8bf8ef2521ae096138d586
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2022-11-07 22:56:04 -0300

    add indeferidas

--- pages/vestibulinho.tsx
+++ pages/vestibulinho.tsx
@@ -10,14 +10,21 @@ const Vestibulinho: React.FC = () => {
 
       <main className={styles.vestibulinho}>
         <h1>Vestibulinho 2023</h1>
+        <p className={styles.descricao}>
+          <u>Técnico em Informática concomitante com Ensino Médio</u>
+          <br />
+          Período integral com duração de 3 anos
+          <br />
+          83 vagas, sendo 3 para pessoas com deficiência
+          <br />
+          <br />
+        </p>
+
         <h2>
-          <u>Técnico em Informática concomitante com Ensino Médio</u><br />
-          Período integral com duração de 3 anos<br />
-          83 vagas, sendo 3 para pessoas com deficiência<br />
-        </h2>
-        
-        <h2>
-          08/11: Inscrições indeferidas <a href= "Indeferidos2023.pdf">veja aqui</a><br /><br />
+          08/11: Inscrições indeferidas{' '}
+          <a href="Indeferidos2023.pdf">veja aqui</a>
+          <br />
+          <br />
         </h2>
         <h2>
           Veja o Edital completo <a href="Edital_2023.pdf">aqui</a>
